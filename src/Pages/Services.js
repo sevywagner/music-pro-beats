21cdab1a78151b192d4f7c4344c32008fb6e811c
Sevy Wagner <sevywagner@Sevys-MBP.attlocal.net>
2022-10-30 18:42:04 -0500

    Added to the services page

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,6 +1,8 @@
 import { cartActions } from '../Store/Redux/cart-slice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import styles from './Css/services.module.css';
+import { motion } from 'framer-motion';
 
 const Services = () => {
     const dispatch = useDispatch();
@@ -14,9 +16,27 @@ const Services = () => {
     }, [dispatch, soundRef]);
 
     return (
-        <div>
-            <p>Services</p>
-        </div>
+        <motion.div
+            initial={{transform: "translateX(150vh)"}}
+            animate={{transform: "translateX(0vh)"}}
+            exit={{}}
+        >
+            <p className={styles.title}>Services</p>
+
+            <div className={styles.service}>
+                <p className={styles.subtitle}>Vocal Engineering</p>
+                <p>
+                    Send me your vocal stems dry and I will return it to you mixed AND mastered(not just a soft clipper on the master) so you can have it ready to send right upon recieving it.
+                </p>
+            </div>
+
+            <div className={styles.service}>
+                <p className={styles.subtitle}>Custom beat</p>
+                <p>
+                    Given any reference, be it an artist, or just a song, or even just "_____ type beat" I can make you something from scratch, no payment until you know you like the beat.
+                </p>
+            </div>
+        </motion.div>
     );
 }
 
